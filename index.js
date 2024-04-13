8fee7995a17c3b5be3ab9115223d5b143cffef94
Way <159807854+Way-00@users.noreply.github.com>
2024-04-13 17:34:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["join [JAGA](https://discord.gg/)","[socials](https://konect.gg/jagafps)"];
 
 
 let currentIndex = 0;
